c4800ecd93e1b2a1e79af7601162b6d610801dba
Quardruple <jokeretaaye@gmail.com>
2021-01-07 17:55:22 +0700

    Design Product New

--- src/app/modules/product/New/product-new.component.ts
+++ src/app/modules/product/New/product-new.component.ts
@@ -8,4 +8,30 @@ import { Component } from '@angular/core';
 
 export class ProductNewComponent {
 
+    switchTab(tabid) {
+        if (!!tabid) {
+            var listtab = document.querySelectorAll(".tab");
+            if (listtab.length > 0) {
+                listtab.forEach(function(elem) {
+                    if (tabid == elem.id) {
+                        elem.classList.add("active");
+                    }
+                    else
+                    {
+                        elem.classList.remove("active");
+                    }
+                });
+            }
+
+            var tabpane = document.getElementsByClassName('tab-content')[0].querySelectorAll(".tab-pane");
+            if (tabpane.length > 0) {
+                tabpane.forEach(element => {
+                    if (tabid == element.id)
+                        element.classList.add("active");
+                    else
+                        element.classList.remove("active");
+                });
+            }
+        }
+    }
 }
